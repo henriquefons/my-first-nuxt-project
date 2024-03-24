6d4e21c50d84aeba6b4180b4849519a21e04b7cf
Henrique Fonseca <henriquefons@hotmail.com>
2024-03-24 12:32:56 -0300

    use  for get api

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,14 +1,36 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+
+const query = ref("");
+const movies = ref([]);
+
+async function onSearch() {
+  const apiKey = config.public.apiKey;
+  const baseURL = config.public.apiUrl;
+  const data: any = await $fetch(baseURL, {
+    params: {
+      apiKey,
+      s: query.value,
+    },
+  });
+
+  console.log(data.Search)
+  movies.value = data.Search
+}
+</script>
 
 <template>
-  <div>
-    Page: movies/index
-  </div>
+  <form @submit.prevent="onSearch">
+    <input type="text" v-model="query">
+    <button> Search </button>
+  </form>
 
   <ul>
-    <li><NuxtLink to="/movies/1"> Go To movie ID: 1 </NuxtLink></li>
-    <li><NuxtLink to="/movies/2"> Go To movie ID: 2 </NuxtLink></li>
-    <li><NuxtLink to="/movies/3"> Go To movie ID: 3 </NuxtLink></li>
+    <li v-for="movie in movies" :key="movie.imdbID">
+      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }"> 
+        {{movie.Title}} 
+      </NuxtLink>
+    </li>
   </ul>
 </template>
 
